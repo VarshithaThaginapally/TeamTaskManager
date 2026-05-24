88782ff0828fe73a272ed4c678e84d91616219b3
Varshitha THaginapally <vthaginapally@gmail.com>
2026-05-24 15:35:28 +0530

    updated backend url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://teamtaskmanager-production.up.railway.app/api"
 });
 
 export default client;
